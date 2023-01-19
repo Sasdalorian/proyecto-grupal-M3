260ceb188c5ce448e371154e5f4d306e2e042471
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-01-19 10:32:46 -0300

    Empezando de 0 por tercera vez -Sas

--- script/contacto.js
+++
@@ -1,8 +0,0 @@
-
-
-function botonalerta(){
-    var comentario=document.forms["contactame"]["comentarios"].value;
-    if (comentario=="") {
-    alert('Debe escribir mensaje antes de enviar');
-}
-}
