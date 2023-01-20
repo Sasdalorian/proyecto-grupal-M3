91d703bbe3a94a2889c1334da338edcf83f11523
juan <juan.salas.salas@outlook.com>
2023-01-20 13:31:26 -0300

    Juan - implementacion carro

---
+++ js/carro.js
@@ -0,0 +1,19 @@
+
+var carro = [];
+
+function agregarProducto(){
+    
+}
+
+
+
+
+function creacionCarro(){
+    var contenedor = document.createElement("div");
+    contenedor.className = "container";
+
+
+
+
+    $("#contenidoCarro").append(contenedor)
+}
