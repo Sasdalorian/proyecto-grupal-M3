b1995141be50e760a508fa1c4346592fa085eaef
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-01-23 13:15:07 -0300

    arreglo index y js carro -Sas

--- js/carro.js
+++ js/carro.js
@@ -1,62 +1,13 @@
-import { infoProducto } from "./cards";
-
-const carrito = document.getElementById("carrito");
-const listaCarrito = document.querySelector("#lista-carrito");
-const vaciarCarrito = document.getElementById("#vaciar-carrito");
-
-cargarEventos();
-
-function cargarEventos() {
-  productos.addEventListener('click', agregarCarrito);
-  carrito.addEventListener('click', eliminar);
-  vaciarCarrito.addEventListener('click', vaciar);
-  document.addEventListener('DOMContentLoaded', leerLocarStorage);
-};
-
-function agregarCarrito(e) {
-  e.preventDefault();
-  if(e.target.classList.contains('bg-primary')) {
-    const producto = e.target.parentElement.parentElement;
-    leerDatos(producto);
-  };
-};
-
-function leerDatos(producto){
-  const infoProductos = {
-    imagen: producto.querySelector('imagenCard').src,
-    nombre: producto.querySelector('card-title').textContent,
-    precio: producto.querySelector('precio').textContent,
-  }
-
-  insertarCarrito(infoProductos);
-};
-
-function insertarCarrito(producto) {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <td>
-            <img src="${infoProducto.imagen}" width: 100> 
-        </td>
-        <td>${infoProducto.nombre}</td>
-        <td>${infoProducto.precio}</td>
-        <td>
-            <a href="#" class="vaciar-carrito" productoCodigo="${infoProducto.codigo}">X</a>
-        </td>
-    `;
-    listaCarrito.appendChild(row);
-    guardarCarritoLocalStorage(producto);
-};
-
-function guardarCarritoLocalStorage(e) {
-    e.preventDefault();
-
-    let producto,
-        productoCodigo;
-
-    if (e.target.classList.contains('vaciar-carrito')) {
-        e.target.parentElement.parentElement.remove();
-        producto = e.target.parentElement.parentElement;
-        productocodigo = producto.querySelector('a').getAtributte('codigo');
-    }
-    eliminarProductoLocalStorage(productoCodigo);
+// FALTA DESIGNARLE VARIABLE A CARRITOESTAVACIO 
+if (carritoEstaVacio) {
+    document.getElementsByClassName('carrito__lista')
+    [0].style.display = "none";
+    document.getElementsByClassName('vacio')
+    [0].style.display = "block"
+}
+if (!carritoEstaVacio) {
+    document.getElementsByClassName('carrito__lista')
+    [0].style.display = "block";
+    document.getElementsByClassName('vacio')
+    [0].style.display = "none";
 }
